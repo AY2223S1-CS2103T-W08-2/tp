9d09d577b01492cedf0bd79049ae871bbfc3493c
Vshnv2001 <mvaishnav.sg@gmail.com>
2022-10-13 17:19:48 +0800

    Add check for non-null GithubUser

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -149,7 +149,7 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, email, tags);
+            return CollectionUtil.isAnyNonNull(name, phone, email, tags, githubUser);
         }
 
         public void setName(Name name) {
